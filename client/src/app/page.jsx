e3b16a0896ee5307bb1c1ed20ba20c306b9b2c2e
Aditya-tech1 <adityatech1m@gmail.com>
2025-03-08 22:43:00 +0530

    done upto education

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -3,7 +3,6 @@ import Education from "@/components/Education";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Project from "@/components/Project";
-import Image from "next/image";
 
 export default function Home() {
   return (
